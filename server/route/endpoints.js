6f2241687b14c1b78ca27e2654b0a30ba0f88438
ABHISHEK VISHWAKARMA <43133456+Abhishek765@users.noreply.github.com>
2021-05-12 19:56:17 +0530

    Updates in endpoints.js

--- server/route/endpoints.js
+++ server/route/endpoints.js
@@ -5,16 +5,27 @@ const router = express.Router();
 // ! Logic for Encoding Decoding
 let hasMap = new Map();
 
+let lastRand, randNum;
+// For generating random number using date 
+function rand() {
+    while (randNum == lastRand)
+        randNum = (new Date().getTime() * 3.141592) % 1;
+
+    return lastRand = randNum;
+}
+
+// To get the random string of length len
 const generateRandomStr = (len, chars) => {
     let result = '';
     for (let i = len; i > 0; --i)
-        result += chars[Math.floor(Math.random() * chars.length)];
+        result += chars[(rand() * chars.length) >>> 0];
     return result;
 }
 // For encoding
 const encodeStr = (longStr) => {
     let sample = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     let shortStr = generateRandomStr(5, sample);
+    console.log(`ShortStr ${shortStr}`);
     hasMap[shortStr] = longStr;
     return shortStr;
 }
@@ -24,11 +35,10 @@ const decode = (shortStr) => {
     return hasMap[shortStr];
 }
 
-
 //! ROUTES
-router.get('/', (req, res) => {
-    res.status(200).send(`Home Welcome to Home`);
-})
+// router.get('/', (req, res) => {
+//     res.status(200).send(`Home Welcome to Home`);
+// })
 
 router.post('/shorten', (req, res) => {
 
